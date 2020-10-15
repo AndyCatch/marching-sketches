611df3ffa0b711f197dad90cd2a121da36f356fc
Andy Catch <andrew.catch@gmail.com>
2020-10-15 10:51:22 -0400

    re-org of sketches

--- sketches/twisty-glue-01.js
+++ sketches/twisty-glue-01.js
@@ -1,15 +1,11 @@
 //  From @khoparzi's examples
 //
 
-march(
-    tw = Twist(ju = Julia())
-    .material('glue')
-  )
-  .render('fractal.low')
+march((tw = Twist((ju = Julia())).material("glue")))
+  .render("fractal.low")
   .camera(0, 0, 3)
 
-onframe = t =>
-{
+onframe = (t) => {
   ju.fold = sin(t * 0.125)
   tw.amount = Vec3(sin(t))
   ju.rotate(t * 30, 1, 1, 0)
